ba9b87b9df403f8cc82e75666dc3f8362791b68c
unwiredo <kentu.dsk@gmail.com>
2025-08-23 17:10:29 +0100

    Update23/08/2025

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+class Game{
+    button5 = document.querySelector('#button0');
+    button1 = document.querySelector('#button1');
+    button2 = document.querySelector('#button2');
+    button3 = document.querySelector('#button3');
+    button4 = document.querySelector('#button4');
+}
